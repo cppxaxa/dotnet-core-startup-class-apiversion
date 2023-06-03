3ee53a0146880ec8fac3e07ec73eb4c4022ee0ad
Prateek Xaxa <prateekxaxa@microsoft.com>
2023-06-03 15:59:27 +0530

    Added source code

---
+++ ApiVersionSample/Constants/ApiVersions.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiVersionSample.Constants
+{
+    public static class ApiVersions
+    {
+        public const string V2023_06_03_preview = "2023-06-03-preview";
+    }
+}
---
+++ ApiVersionSample/Models/ApiVersioningErrorResponse.cs
@@ -0,0 +1,38 @@
+﻿namespace ApiVersionSample.Models
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Versioning;
+    using System;
+    using System.Linq;
+    using System.Text.Encodings.Web;
+
+    internal class ApiVersioningErrorResponse : DefaultErrorResponseProvider
+    {
+        private readonly HtmlEncoder htmlEncoder = HtmlEncoder.Default;
+
+        public override IActionResult CreateResponse(ErrorResponseContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var sanitizedContext = new ErrorResponseContext(
+                context.Request,
+                context.StatusCode,
+                context.ErrorCode,
+                htmlEncoder.Encode(context.Message) + " " + GetApiVersionsMessage(),
+                context.MessageDetail);
+            return base.CreateResponse(sanitizedContext);
+        }
+
+        private string GetApiVersionsMessage()
+        {
+            string apiVersionCatalog = string.Join(
+                ",",
+                typeof(Constants.ApiVersions).GetFields().Select(e => $"'{e.GetValue(null)}'"));
+            string message = $"The supported api versions are {apiVersionCatalog}.";
+            return message;
+        }
+    }
+}
